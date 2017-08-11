c688310a3c9b50db83ee1b67a917441b71d0779a
azoladyantyi <aizydyantyi@gmail.com>
2017-08-11 12:48:16 +0200

    commit the changes i made on my index.js file

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var app = express();
 
 // create a route
-app.get('/Hello', function (req, res) {
- res.send('Hello Joe');
+app.get('/', function (req, res) {
+ res.send('Hello World!');
+});
+app.get('/greetings', function (req, res) {
+ res.send('Hello, Joe');
 });
 
 //start the server
@@ -12,6 +15,6 @@ var server = app.listen(3000, function () {
  var host = server.address().address;
  var port = server.address().port;
 
- console.log('http://localhost:3000/hello', host, port);
+ console.log('Example app listening at http://%s:%s', host, port);
 
 });
