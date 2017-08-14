4788f29a6b071237335c56e4482107f237f638dc
azoladyantyi <aizydyantyi@gmail.com>
2017-08-14 12:47:46 +0200

    added counter on my file index.js

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
 var express = require('express');
 var app = express();
+var list = [];
+var avoidDublicate = [];
+
 
 // create a route
-app.get('/', function (req, res) {
- res.send('Hello World!');
+app.get('/', function(req, res) {
+    res.send('Hello World!');
 });
-app.get('/greetings/:users', function (req, res) {
- res.send('Hello ' + req.params.users);
 
+app.get('/greetings/:users', function(req, res) {
+    res.send('Hello ' + req.params.users);
+    var users = req.params.users
+    if (avoidDublicate.indexOf(users) === -1) {
+        avoidDublicate.push(users)
+        list.push(users);
+    };
+});
+app.get('/greeted', function(req, res) {
+    res.send(avoidDublicate);
+
+});
+app.get('/counter/:users', function(req, res) {
+    var users = req.params.users;
+    //creat a variable and asign it as 0
+    var counter = 0;
+    //createa for loop
+    for (var i = 0; i < list.length; i++) {
+        if (list[i].users = users) {
+            counter++;
+        }
+    }
+    res.send(users + " " + "has been greeted " + counter++ + " " + 'times')
 });
 
 //start the server
-var server = app.listen(3000, function () {
+var server = app.listen(3000, function() {
 
- var host = server.address().address;
- var port = server.address().port;
+    var host = server.address().address;
+    var port = server.address().port;
 
- console.log('Example app listening at http://%s:%s', host, port);
-
+    console.log('Example app listening at http://%s:%s', host, port);
 });
